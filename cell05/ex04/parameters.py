c359ac43601b1e3c2934bdc8bad709fe7fca23b0
TeemeeNa <kiachtiphongnakub@gmail.com>
2026-04-25 07:59:16 +0000

    edit folder name cell04

--- cell05/ex04/parameters.py
+++ cell05/ex04/parameters.py
@@ -1,9 +1,3 @@
-A = input("")
-B = A.split()
-C = len(B)
-count = 0
-while count != C:
-    print(B[count],end=" ")
-    count += 1
-print("")
-print(f"Number of parameters: {C}")
+A = input("What you gotta say? : ")
+while A != "STOP":
+    A = input("I got that! Anything else?: ")
